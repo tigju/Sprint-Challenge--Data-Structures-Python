f2501a723524d9caae91a9dbc3bd80c517cd0582
tigju <tigju24@gmail.com>
2020-09-26 14:47:36 -0400

    names challenge

--- names/names.py
+++ names/names.py
@@ -18,33 +18,32 @@ duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
 # O(n^2)
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
-end_time = time.time()
-print (f"\n{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime of nested loop (O(n^2)): {end_time - start_time} seconds")
+# end_time = time.time()
+# print (f"\n{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+# print (f"runtime of nested loop (O(n^2)): {end_time - start_time} seconds")
 
 ################################################################
 # Using linked lists data structures to reduce time complexity 
 
+
 duplicates1 = LinkedList()  # Return the list of duplicates in this data structure
-names_list1 = LinkedList()
-names_list2 = LinkedList()
+
 start_time1 = time.time()
 
-# for name_1 in names_1:
-#     names_list1.add_to_tail(name_1)
 
-# for name_2 in names_2:
-#     names_list2.add_to_tail(name_2)
+for name_1 in names_1:  # O(n)
+    if name_1 in names_2:
+        duplicates1.add_to_tail(name_1)
 
-# duplicates1.compare(names_list1, names_list2)
-# end_time1 = time.time()
-# print (f"\n{duplicates1.length} duplicates:\n\n{duplicates1.output_list()}\n\n")
-# print (f"runtime of for loop with Linked list: {end_time1 - start_time1} seconds")
+
+end_time1 = time.time()
+print(f"\n{duplicates1.length} duplicates:\n\n{', '.join(duplicates1.output_list())}\n\n")
+print (f"runtime of for loop with Linked list: {end_time1 - start_time1} seconds")
 
 
 
@@ -57,52 +56,52 @@ start_time1 = time.time()
 
 # # O(n)
 
-duplicates2 = []
-start_time2 = time.time()
-for name_1 in names_1: # O(n)
-    if name_1 in names_2:  # O(1) + O(n)
-        duplicates2.append(name_1)  # O(1)
-end_time2 = time.time()
-print(f"\n{len(duplicates2)} duplicates:\n\n{', '.join(duplicates2)}\n\n")
-print(f"runtime of for loop and if statement (O(n)): {end_time2 - start_time2} seconds")
+# duplicates2 = []
+# start_time2 = time.time()
+# for name_1 in names_1: # O(n)
+#     if name_1 in names_2:  # O(1) + O(n)
+#         duplicates2.append(name_1)  # O(1)
+# end_time2 = time.time()
+# print(f"\n{len(duplicates2)} duplicates:\n\n{', '.join(duplicates2)}\n\n")
+# print(f"runtime of for loop and if statement (O(n)): {end_time2 - start_time2} seconds")
 
 ###################################################################################
 
 # # O(n) with count() function
 
-duplicates3 = []
-start_time3 = time.time()
-for name_1 in names_1:  # O(n)
-    if names_2.count(name_1) > 0:  # O(1) + O(n)
-        duplicates3.append(name_1)  # O(1)
-end_time3 = time.time()
-print(f"\n{len(duplicates3)} duplicates:\n\n{', '.join(duplicates3)}\n\n")
-print(f"runtime of for loop with count(): {end_time3 - start_time3} seconds")
+# duplicates3 = []
+# start_time3 = time.time()
+# for name_1 in names_1:  # O(n)
+#     if names_2.count(name_1) > 0:  # O(1) + O(n)
+#         duplicates3.append(name_1)  # O(1)
+# end_time3 = time.time()
+# print(f"\n{len(duplicates3)} duplicates:\n\n{', '.join(duplicates3)}\n\n")
+# print(f"runtime of for loop with count(): {end_time3 - start_time3} seconds")
 
 ################################################################################
 
 # List comprehension 
 
-start_time4 = time.time()
-duplicates4 = [name for name in names_1 if name in names_2]  # O(n)
-end_time4 = time.time()
-print(f"\n{len(duplicates4)} duplicates:\n\n{', '.join(duplicates4)}\n\n")
-print(f"runtime of list comprehension: {end_time4 - start_time4} seconds")
+# start_time4 = time.time()
+# duplicates4 = [name for name in names_1 if name in names_2]  # O(n)
+# end_time4 = time.time()
+# print(f"\n{len(duplicates4)} duplicates:\n\n{', '.join(duplicates4)}\n\n")
+# print(f"runtime of list comprehension: {end_time4 - start_time4} seconds")
 
 #############################################################################
 
 # # most efficient way with Sets Intersection (&)
 
-start_time5 = time.time()
-def find_dups(n1, n2):
-    names_set1 = set(n1)  # O(len(s))
-    names_set2 = set(n2)  # O(len(s))
+# start_time5 = time.time()
+# def find_dups(n1, n2):
+#     names_set1 = set(n1)  # O(len(s))
+#     names_set2 = set(n2)  # O(len(s))
 
-    if (names_set1 & names_set2): # O(min(len(s1), len(s2)))
-        return (names_set1 & names_set2) # O(min(len(s1), len(s2)))
+#     if (names_set1 & names_set2): # O(min(len(s1), len(s2)))
+#         return (names_set1 & names_set2) # O(min(len(s1), len(s2)))
 
-duplicates5 = find_dups(names_1, names_2)
+# duplicates5 = find_dups(names_1, names_2)
 
-end_time5 = time.time()
-print(f"\n{len(duplicates5)} duplicates:\n\n{', '.join(duplicates5)}\n\n")
-print(f"runtime funcion with sets intersection: {end_time5 - start_time5} seconds")
+# end_time5 = time.time()
+# print(f"\n{len(duplicates5)} duplicates:\n\n{', '.join(duplicates5)}\n\n")
+# print(f"runtime funcion with sets intersection: {end_time5 - start_time5} seconds")
