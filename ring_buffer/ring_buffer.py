c81b410afe1ab8b24b22f1c956750c3eed16df1a
tigju <tigju24@gmail.com>
2020-09-26 13:32:47 -0400

    ring buffer challenge

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -22,13 +22,12 @@
 #         return [i for i in self.buff if i is not None]
 
 
-# Circular Linked List implementation
+# Singly Linked List data structure implementation
 
 class Node:
     def __init__(self, value, next_node=None, prev_node=None):
         self.value = value
         self.next_node = next_node
-        self.prev_node = prev_node
 
 
 class RingBuffer:
@@ -55,22 +54,27 @@ class RingBuffer:
         # if capacity equals or greater than size
         else:
             if self.pointer == self.tail:
+                self.pointer = self.head
+                self.tail = new_node
+                new_node.next_node = self.pointer
+            if self.pointer == self.head:
                 self.pointer = self.head.next_node
                 self.head = new_node
-            new_node.next_node = self.pointer
-            
-            print("current pointer", self.pointer.value)
-            
-            old_pointer_next = self.pointer.next_node
-            self.pointer = new_node
-            self.pointer = old_pointer_next
-                         
-            print("next pointer", self.pointer.value)
+                new_node.next_node = self.pointer
+            else:
+                # print("current pointer", self.pointer.value)
+                if self.pointer.next_node == None:
+                    old_pointer_next = self.head
+                else:
+                    old_pointer_next = self.pointer.next_node
+                
+                self.pointer.value = new_node.value
+                self.pointer = old_pointer_next
+                            
+                # print("next pointer", self.pointer.value)
+                # print("new node", new_node.value)
 
-            print("new node", new_node.value)
-
             
-
     def get(self):
         l = []
         curr = self.head
@@ -100,4 +104,12 @@ print(r.get())
 r.append('4')
 print(r.get())
 r.append('5')
+print(r.get())
+r.append('6')
+print(r.get())
+r.append('7')
+print(r.get())
+r.append('8')
+print(r.get())
+r.append('9')
 print(r.get())
